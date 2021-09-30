8f8f89a551234282b0c98f65962a9b29fbe7fe82
p0int-bl4nk <skv860254262@gmail.com>
2021-09-30 08:52:08 +0530

    exercises 3.1-3.6

---
+++ part3/phonebook-server/index.js
@@ -0,0 +1,102 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+let persons = [
+  {
+    "name": "Arto Hellas",
+    "number": "040-123456",
+    "id": 1
+  },
+  {
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523",
+    "id": 2
+  },
+  {
+    "name": "Dan Abramov",
+    "number": "12-43-234345",
+    "id": 3
+  },
+  {
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122",
+    "id": 4
+  }
+];
+
+const generateId = () => {
+  const max = 100000;
+  const min = 999999;
+  while (true) {
+    const randomId = Math.floor(Math.random() * (max - min + 1)) + min;
+    if (!persons.some(person => person.id === randomId)) {
+      return randomId;
+    }
+  }
+}
+
+app.get('/info', (req, res) => {
+  const response = `
+    <p>Phonebook has info for ${persons.length} people.</p>
+    <p>${new Date()}</p>
+  `;
+  res.send(response);
+});
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons);
+});
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find(person => person.id === id);
+  if (person) {
+    return res.json(person);
+  }
+  res.status(404).end();
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter(person => person.id !== id);
+  res.status(204).end();
+});
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body;
+
+  if (!body.name) {
+    return res.status(400).json({
+      error: `name missing`
+    });
+  }
+
+  if (!body.number) {
+    return res.status(400).json({
+      error: `number missing`
+    });
+  }
+
+  if (persons.some(person => person.name === body.name)) {
+    return res.status(400).json({
+      error: 'name must be unique'
+    });
+  }
+
+  const newPerson = {
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  };
+
+  persons.push(newPerson);
+
+  res.json(newPerson);
+});
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
