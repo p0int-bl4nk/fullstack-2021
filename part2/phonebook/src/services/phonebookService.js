8d2e4084bf23ea162d9e78e2023088ed9f5122f2
p0int-bl4nk <skv860254262@gmail.com>
2021-10-03 13:58:33 +0530

    added backend url

--- part2/phonebook/src/services/phonebookService.js
+++ part2/phonebook/src/services/phonebookService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = 'http://localhost:3001/api';
+const baseUrl = 'https://young-reaches-34491.herokuapp.com/api';
 
 const getAll = () => {
   return axios.get(`${baseUrl}/persons`).then(response => response.data);
