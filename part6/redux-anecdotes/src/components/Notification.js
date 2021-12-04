eaa9bee6dc654c58b3d5ddb1daed013c2c18ddd2
p0int-bl4nk <skv860254262@gmail.com>
2021-12-04 14:24:57 +0530

    exercises 6.19

--- part6/redux-anecdotes/src/components/Notification.js
+++ part6/redux-anecdotes/src/components/Notification.js
@@ -1,27 +1,35 @@
 
 import React from 'react'
-import {useDispatch, useSelector} from "react-redux";
+import {connect} from "react-redux";
 import {actionHideNotification} from "../reducers/notificationReducer";
 
-const Notification = () => {
-  const dispatch = useDispatch();
-  const notification = useSelector(state => state.notification);
+const Notification = (props) => {
+
   const style = {
     border: 'solid',
     padding: 10,
     borderWidth: 1
   }
 
-  if (!notification)
+  if (!props.notification)
     return null;
   else
-    setTimeout(() => dispatch(actionHideNotification()), 3000);
+    setTimeout(() => props.actionHideNotification(), 3000);
 
   return (
     <div style={style}>
-      {notification}
+      {props.notification}
     </div>
   )
 }
 
-export default Notification
+const mapStateToProps = ({ notification }) => ({ notification });
+
+const mapDispatchToProps = { actionHideNotification }
+
+const ConnectedNotification = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Notification);
+
+export default ConnectedNotification
